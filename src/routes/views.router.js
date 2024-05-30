c913f2101367973258131d39e60f77b1c83e2f5e
Fer88Cabral <fer88encoder@outlook.com>
2024-05-30 15:38:37 -0400

    Implementación de login 2.0

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,11 +1,11 @@
 import { Router } from 'express'; 
 import {LoginPost, cartIdView, chatView, homeView, loginGet, Logout, productsView, realTimeProductsView, registerGet, registerPost} from '../controllers/views.js';
-import { auth } from '../middleware/auth.js';
+import { admin, auth } from '../middleware/auth.js';
 
 const router = Router();
 
-router.get('/',  auth, homeView);
-router.get('/realtimeproducts', auth, realTimeProductsView);
+router.get('/', homeView);
+router.get('/realtimeproducts', [auth, admin], realTimeProductsView);
 router.get('/chat',  auth, chatView);
 router.get('/products',  auth, productsView);
 router.get('/cart/:cid',  auth, cartIdView);
@@ -13,7 +13,6 @@ router.get('/login', loginGet);
 router.post('/login', LoginPost);
 router.get('/register', registerGet);
 router.post('/register', registerPost);
-
 router.get('/logout', Logout);
 
 export default router;  
